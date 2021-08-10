d4722b1dc71aed5c351f532bc7bcf1891e7f6118
phCamelo <pauloh.cg@hotmail.com>
2021-08-10 20:20:33 -0300

    Primeiro Exercício de Front 2

--- Aula01/js/main.js
+++ Aula01/js/main.js
@@ -7,25 +7,25 @@ notaSegundo = Number.parseFloat(notaSegundo);
 let notas = [notaPrimeiro, notaSegundo]; // cria um array com a nota do primeiro e segundo bimestre
 let somaSegundoBimestre = notas.reduce((acc, elem) => acc + elem); // soma as notas do array
     console.log(
-  `A soma das notas dos 2 primeiros bimestres é: ${somaSegundoBimestre}`
+  `A soma das notas dos 2 primeiros bimestres é: ${somaSegundoBimestre.toFixed(2)}`
 );
 
 let notaTerceiro = prompt("Digite a nota do Terceiro Bimestre");
 notaTerceiro = Number.parseFloat(notaTerceiro);
 
-notas.push(notaTerceiro);
+notas.push(notaTerceiro);// adiciona a nota do terceiro bimestro ao array notas
 let soma3Bimestres = notas.reduce((acc, elem) => acc + elem);
-    console.log(`A soma das notas dos 3 primeiros bimestres é: ${soma3Bimestres}`);
+    console.log(`A soma das notas dos 3 primeiros bimestres é: ${soma3Bimestres.toFixed(2)}`);
 
 let notaQuarto = prompt("Digite a nota do Quarto Bimestre");
 notaQuarto = Number.parseFloat(notaQuarto);
 
 notas.push(notaQuarto);
 let somaTotal = notas.reduce((acc, elem) => acc + elem);
-let media = somaTotal / 4;
+let media = somaTotal / notas.length;
 
 if (media >= 7) {
-  console.log(`A média final dos 4 Bimestres é: ${media}, foi aprovado`);
+  console.log(`A média final dos 4 Bimestres é: ${media.toFixed(2)}, você foi aprovado 😀`);
 } else if (media < 7) {
-  console.log(`A média final dos 4 Bimestres é: ${media}, foi reprovado`);
+  console.log(`A média final dos 4 Bimestres é: ${media.toFixed(2)}, foi reprovado 🙁`);
 }
